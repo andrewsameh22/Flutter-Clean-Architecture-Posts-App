f54c8cf99ebaf7fc5bee8b4cb230fc07800865ce
Andrew Sameh <andrewsameh22@hotmail.com>
2024-05-03 17:08:29 +0300

    post model

---
+++ lib/features/posts/data/models/post_model.dart
@@ -0,0 +1,25 @@
+import '../../domain/entities/post_entity.dart';
+
+class PostModel extends Post {
+  PostModel({
+    required super.id,
+    required super.title,
+    required super.body,
+  });
+
+  factory PostModel.fromJson(Map<String, dynamic> json) {
+    return PostModel(
+      id: json['id'],
+      title: json['title'],
+      body: json['body'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = Map<String, dynamic>();
+    data['id'] = id;
+    data['title'] = title;
+    data['body'] = body;
+    return data;
+  }
+}
